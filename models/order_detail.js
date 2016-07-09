6bd7c47b0b330404a42f23c47f26db63caf4a75c
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-07-10 00:55:47 +0200

    Fix validation error messages in order_detail model

--- models/order_detail.js
+++ models/order_detail.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false,
         validate: {
           notEmpty: {
-            msg: "Total is required"
+            msg: "Total price is required"
           }
         }
       },
