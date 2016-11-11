15ff69895f00971972e2b60c022ea3aab7823a44
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-11-11 22:30:44 +0100

    Angular: default sort item by ID

--- public/javascripts/items/item-list/item-list.component.js
+++ public/javascripts/items/item-list/item-list.component.js
@@ -4,7 +4,7 @@ angular.
     templateUrl: '/items/partials/list',
     controller: ['Item',
       function ItemListController(Item) {
-        this.orderProp = 'default';
+        this.orderProp = '-id';
         //when items list returned in the callback, 'this' is not defined, so 'self' point back to the controller instance.
         //var self = this;
         //$http.get('/api/items/').then(function(response) {
