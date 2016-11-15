c079bc8f8ae5de72bef51e89851d127e20d69a33
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-11-15 16:02:33 +0100

    Instock.filter.js: change the output

--- public/javascripts/core/instock/instock.filter.js
+++ public/javascripts/core/instock/instock.filter.js
@@ -2,6 +2,6 @@ angular.
   module('core').
   filter('instock', function() {
     return function(input) {
-      return input > 0 ? '\u2713' : '\u2718';
+      return input > 0 ? '\u2713 (' + input + ' pieces)' : '\u2718';
     };
   });
