25cd2bbd8f59f6c06f764886cf24dc853811dab5
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2016-11-19 12:21:12 +0100

    Accounts API: refactor the error format

--- api/accounts.js
+++ api/accounts.js
@@ -2,15 +2,10 @@ var express = require('express');
 var api = express.Router();
 
 //------------------- Admin Section ----------------------
-//--------------------------------------------------------
-
-//----------------- Authenticated section --------------------
 api.get('/',
   function(req, res, next) {
-    if (!res.locals.authenticated || !res.locals.isAdmin) {
-      var err = new Error('You are not permitted to access this!');
-      err.status = 401;
-      return next(err);
+    if (!res.locals.isAdmin) {
+      return res.status(401).json({msg: 'You are not permitted to access this!'}); 
     }
     next();
   }, function(req, res, next) {
@@ -19,18 +14,20 @@ api.get('/',
     .then(function(accounts){
       return res.json(accounts);
     }).catch( function(error){
-      return next(error);
+      return res.status(400).json(error); 
     });
 });
+//--------------------------------------------------------
 
+//----------------- Authenticated section --------------------
 api.post('/',
   function(req, res, next) {
     if (res.locals.authenticated && !res.locals.isAdmin) {
-      var err = new Error('You are not permitted to access this!');
-      err.status = 401;
-      return next(err);
+      return res.status(401).json({msg: 'You are not permitted to access this!'}); 
     }
-    if (!req.body) return next(new Error('Cannot get the req.body'));
+    if (!req.body) {
+      return res.status(400).json({msg: 'Cannot get the req.body'}); 
+    }
 
     next();
   }, function(req, res, next) {
@@ -58,9 +55,7 @@ api.post('/',
 api.delete('/:id',
   function(req, res, next) {
     if (!res.locals.isAdmin) {
-      var err = new Error('You are not permitted to access this!');
-      err.status = 401;
-      return next(err);
+      return res.status(401).json({msg: 'You are not permitted to access this!'}); 
     }
 
     next();
@@ -77,6 +72,7 @@ api.delete('/:id',
   }
 );
 
+/*TODO: handle customer and admin views*/
 api.get('/:id', 
   function (req, res, next) {
     if (!res.locals.authenticated) {
@@ -90,30 +86,34 @@ api.get('/:id',
     Account.findById(req.params.id, {
       include: [req.models.admin, req.models.customer]
     }).then(function(account) {
-      if (!account) return next(new Error("Can't find the account with id: " + req.params.id));
+      if (!account) {
+        return res.status(404).json({msg: "Can't find the account with id: " + req.params.id}); 
+      }
+
       var returnedAccount = account.toJSON();
       returnedAccount.is_owner = account.id == res.locals.current_account.id;
-      console.log(returnedAccount);
       return res.json(returnedAccount); 
     }).catch(function(error) {
-      return next(error);
+      return res.status(400).json(error); 
     });
 });
 
 api.post('/:id',
   function(req, res, next) {
     if (!res.locals.isAdmin) {
-      var err = new Error('You are not permitted to access this!');
-      err.status = 401;
-      return next(err);
+      return res.status(401).json({msg: 'You are not permitted to access this!'}); 
     }
-    if (!req.body) return next(new Error('Cannot get the req.body'));
+    if (!req.body) {
+      return res.status(400).json({msg: 'Cannot get the req.body'}); 
+    }
     next();
   }, function(req, res, next) {
     var data = req.body;
     var Account = req.models.account;
     Account.findById(data.id).then(function(account) {
-      if (!account) return next(new Error("Can't find the account with id: " + data.id));
+        if (!account) {
+          return res.status(404).json({msg: "Can't find the item with id: " + data.id}); 
+        }
 
       account.update(data).then(function(account){
         return res.json(account); 
